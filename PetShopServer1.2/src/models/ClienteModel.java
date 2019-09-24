f0ad2e2fb83fdf8c7f3243bd3e63c8c66b935288
juanvmr <juan97victor@hotmail.com>
2019-09-24 19:37:36 -0300

    Add files via upload

---
+++ PetShopServer1.2/src/models/ClienteModel.java
@@ -0,0 +1,56 @@
+package models;
+
+public class ClienteModel {
+
+    private String Nome;
+    private String Endereco;
+    private String Email;
+    private String CPF;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String Endereco) {
+        this.Endereco = Endereco;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome = " + Nome + "\nEndereço = " + Endereco + "\nEmail = " + Email + "\nCPF = " + getCPF();
+    }
+
+}
