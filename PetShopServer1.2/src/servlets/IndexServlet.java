c71af07ddef18fa056bbde5f5ff712bb255d1f9f
juanvmr <juan97victor@hotmail.com>
2019-09-26 20:12:30 -0300

    Update IndexServlet.java

--- PetShopServer1.2/src/servlets/IndexServlet.java
+++ PetShopServer1.2/src/servlets/IndexServlet.java
@@ -7,12 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet({"/", "/index.html"})
+@WebServlet("/")
 public class IndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher().forward("/src/serverPages/index.html");
+		request.getRequestDispatcher().forward("/PetShopServer1.2/index.html");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
